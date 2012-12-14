f1e65cc495a16399c3372bc244ca9444b8c2e105
tibiyacks <tibiyacks@a4e74e71-8cbc-d749-3e16-06410a0509a9>
2012-12-14 22:17:24 +0000

    14/12/12 05:17  tibi

--- trunk/site/includes/class/alumno.php
+++ trunk/site/includes/class/alumno.php
@@ -1,13 +1,18 @@
 <?php
 
 
-class alumno{
-    
-      function __construct(){
+class alumno
+{
+
+var $ciclo;
+
+    function __construct()
+    {
     }
-    
-    function listar(){
-        include("includes/conexion.php");
+
+    function listar()
+    {
+        include ("includes/conexion.php");
 
         $rs_list = $cn->query("CALL paAlumnoListar()");
         $row_list = $rs_list->num_rows;
@@ -25,45 +30,50 @@ class alumno{
             <th>E-mail</th>
         </tr>
         ';
-        $i=0;
-        while($rows = $rs_list->fetch_array(MYSQLI_ASSOC))
-        {
+        $i = 0;
+        while ($rows = $rs_list->fetch_array(MYSQLI_ASSOC)) {
             echo "<tr>";
-            echo "<td>".$rows["idalumno"]."</td>";
-            echo "<td>".$rows["nombre_completo"]."</td>";
-            echo "<td>".$rows["dni"]."</td>";
-            echo "<td>".$rows["fecha_nacimiento"]."</td>";
-            echo "<td>".$rows["telefono_fijo"]."</td>";
-            echo "<td>".$rows["celular"]."</td>";
-            echo "<td>".$rows["email"]."</td>";
+            echo "<td>" . $rows["idalumno"] . "</td>";
+            echo "<td>" . $rows["nombre_completo"] . "</td>";
+            echo "<td>" . $rows["dni"] . "</td>";
+            echo "<td>" . $rows["fecha_nacimiento"] . "</td>";
+            echo "<td>" . $rows["telefono_fijo"] . "</td>";
+            echo "<td>" . $rows["celular"] . "</td>";
+            echo "<td>" . $rows["email"] . "</td>";
             echo "</tr>";
             $i++;
-        };
+        }
+        ;
         echo "</table>";
     }
-    
-    function insertar($nombre,$ape,$sex,$dni,$fnac,$dir,$tel,$celu,$mail){
-    include("../conexion.php");
-    $qinsertar = $cn->query("CALL paAlumnosInsertar('".$nombre."', '".$ape."', '".$sex."', '".$dni."', '".$fnac."', '".$dir."', '".$tel."', '".$celu."', '".$mail."')");
-    
-    if($qinsertar)
-        return true;
-    else 
-        return false;
-}
 
-function listarID($id){
-	include("../conexion.php");
+    function insertar($nombre, $ape, $sex, $dni, $fnac, $dir, $tel, $celu, $mail)
+    {
+        include ("../conexion.php");
+        $qinsertar = $cn->query("CALL paAlumnosInsertar('" . $nombre . "', '" . $ape .
+            "', '" . $sex . "', '" . $dni . "', '" . $fnac . "', '" . $dir . "', '" . $tel .
+            "', '" . $celu . "', '" . $mail . "')");
+
+        if ($qinsertar)
+            return true;
+        else
+            return false;
+    }
+
+    function listarID($id)
+    {
+        include ("../conexion.php");
 
         $rs_list = $cn->query("CALL paAlumnoListarID('$id')");
         $row_list = $rs_list->num_rows;
         $rows = $rs_list->fetch_array(MYSQLI_ASSOC);
-        
-        return $rows;
-}
 
-function listarCursosInscritos($idalumnos){
-        include("includes/conexion.php");
+        return $rows;
+    }
+
+    function listarCursosInscritos($idalumnos)
+    {
+        include ("includes/conexion.php");
 
         $rs_list = $cn->query("CALL paAlumnosCursosInscritos('$idalumnos')");
         $row_list = $rs_list->num_rows;
@@ -80,42 +90,52 @@ function listarCursosInscritos($idalumnos){
             <th>Vez</th>
         </tr>
         ';
-        $i=0;
-        while($rows = $rs_list->fetch_array(MYSQLI_ASSOC))
-        {
+        $i = 0;
+        while ($rows = $rs_list->fetch_array(MYSQLI_ASSOC)) {
             echo "<tr>";
-            echo "<td>".$rows["IDCurso"]."</td>";
-            echo "<td>".$rows["nombreCurso"]."</td>";
-            echo "<td>".$rows["ciclo"]."</td>";
-            echo "<td>".$rows["creditos"]."</td>";
-            echo "<td>".$rows["vez"]."</td>";
+            echo "<td>" . $rows["IDCurso"] . "</td>";
+            echo "<td>" . $rows["nombreCurso"] . "</td>";
+            echo "<td>" . $rows["ciclo"] . "</td>";
+            echo "<td>" . $rows["creditos"] . "</td>";
+            echo "<td>" . $rows["vez"] . "</td>";
             echo "</tr>";
             $i++;
-        };
+        }
+        ;
         echo "</table>";
     }
 
-public function modificar($nombre,$apellidos,$sexo,$dni,$fechan,$direccion,$telefono,$celular,$email,$id)
-	{
-		include '../conexion.php';
-		$rs = $cn->query("CALL paAlumnoActualizar('$nombre','$apellidos','$sexo','$dni','$fechan','$direccion','$telefono','$celular','$email','$id')");
-        
-        if($rs)
-            return true;
-        else
-            return false;
-	}
-    
-    function cambiarClave($id,$claveAntigua,$claveNueva)
+    function listarCursosDisponibles($id)
     {
-	   	include '../conexion.php';
-		$rs = $cn->query("CALL paAlumnoCambiarClave('$id', '$claveAntigua', '$claveNueva')");
+        include '../conexion.php';
+        $rsCiclo = $cn->query("SELECT a.cicloActual FROM alumno a WHERE a.idalumno='$id'");
+        $row = $rsCiclo->fetch_array(MYSQLI_ASSOC);
+        $this->ciclo = $row["ciclo"];
         
-        if($rs)
+    }
+
+    public function modificar($nombre, $apellidos, $sexo, $dni, $fechan, $direccion,
+        $telefono, $celular, $email, $id)
+    {
+        include '../conexion.php';
+        $rs = $cn->query("CALL paAlumnoActualizar('$nombre','$apellidos','$sexo','$dni','$fechan','$direccion','$telefono','$celular','$email','$id')");
+
+        if ($rs)
             return true;
         else
             return false;
-}
+    }
+
+    function cambiarClave($id, $claveAntigua, $claveNueva)
+    {
+        include '../conexion.php';
+        $rs = $cn->query("CALL paAlumnoCambiarClave('$id', '$claveAntigua', '$claveNueva')");
+
+        if ($rs)
+            return true;
+        else
+            return false;
+    }
 
 }
 
